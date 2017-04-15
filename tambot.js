d359fc33aa3f221349be090b5235a68f23ee9645
Exait <pyxelation@gmail.com>
2017-04-14 20:23:21 -0400

    Added Connect Message And Game

--- tambot.js
+++ tambot.js
@@ -3,7 +3,8 @@ const Client = new Discord.Client()
 const Config = require('./config.json')
 
 Client.on('ready', () => {
-
+	console.log("Tambot -> Connected")
+	Client.user.setGame('Tamriel')
 })
 
 Client.on('message', msg => {
